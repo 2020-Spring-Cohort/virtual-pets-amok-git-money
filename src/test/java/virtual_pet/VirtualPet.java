f9c9aaac5d5bea4fae58a8bb0f5b546f94876aaa
James Brooks <letstalk@jamesbrooks.tech>
2020-01-17 13:22:36 -0500

    Created VirtualPet class, began Test Driven coding

---
+++ src/test/java/virtual_pet/VirtualPet.java
@@ -0,0 +1,66 @@
+package virtual_pet;
+
+public class VirtualPet {
+    public String petName = "";
+    public String color = "";
+    public int weight = 0;
+    public int boredomLevel = 0;
+    public int health = 10;
+    public int hunger = 10;
+    public int cleanliness = 7;
+    public int thirst = 0;
+
+    public VirtualPet(String petName, String type, String color) {
+        this.petName = petName;
+        this.color = color;
+    }
+
+    public void feed(){
+        hunger-=3;
+        if(hunger < 0){
+            hunger = 0;
+        }
+    }
+    public void play(){
+        boredomLevel-=3;
+        if(boredomLevel < 0){
+            boredomLevel=0;
+        }
+    }
+    public void takeToDoctor(){
+        health+=3;
+        if(health>10){
+            health = 10;
+        }
+    }
+    public void clean(){
+        cleanliness+=3;
+        if(cleanliness > 10){
+            cleanliness = 10;
+        }
+    }
+    public void waterPet(){
+        thirst-=3;
+        if(thirst < 0){
+            thirst = 0;
+        }
+    }
+
+    public int getHunger(){
+        return hunger;
+    }
+
+    public String getName(){
+        return petName;
+    }
+
+    public int getHealth() {
+        return health;
+
+    }
+
+    public int getCleanliness() {
+        return cleanliness;
+    }
+}
+
--- src/test/java/virtual_pet/VirtualPetTest.java
+++ src/test/java/virtual_pet/VirtualPetTest.java
@@ -1,4 +1,63 @@
 package virtual_pet;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VirtualPetTest {
+
+    @Test
+    public void shouldBeAbleToCreatePet() {
+        VirtualPet pet = new VirtualPet("BOB", "robot", "black");
+    }
+
+    @Test
+    public void petShouldHaveAName(){
+        VirtualPet underTest = new VirtualPet("BOB", "robot", "black");
+        String result = underTest.getName();
+        assertEquals("BOB", result);
+    }
+    @Test
+    public void petShouldHaveHungerSetToTen(){
+        VirtualPet underTest = new VirtualPet("BOB", "robot", "black");
+        int result = underTest.getHunger();
+        assertEquals(10, result);
+    }
+    @Test
+    public void feedingPetShouldDecreaseHungerBy3(){
+        VirtualPet underTest = new VirtualPet("BOB", "robot", "black");
+        underTest.feed();
+        int result = underTest.getHunger();
+        assertEquals(7, result);
+    }
+    @Test
+    public void feedingNotBelowZero(){
+        VirtualPet underTest = new VirtualPet("BOB", "robot", "black");
+
+        for(int i = 0; i<5; i++){
+            underTest.feed();
+        }
+        int result = underTest.getHunger();
+        assertEquals(0, result);
+    }
+    @Test
+    public void healthShouldBe10(){
+        VirtualPet underTest = new VirtualPet("BOB", "robot", "black");
+        underTest.getHealth();
+        int result = underTest.getHealth();
+        assertEquals(10, result);
+    }
+    @Test
+    public void cleanlinessShouldBe10(){
+        VirtualPet underTest = new VirtualPet("BOB", "robot", "black");
+        underTest.getCleanliness();
+        int result = underTest.getCleanliness();
+        assertEquals(10, result);
+    }
+    @Test
+    public void cleaningShouldIncreaseCleanliness(){
+        VirtualPet underTest = new VirtualPet("Bob", "robot", "black");
+        underTest.clean();
+        int result = underTest.getCleanliness();
+        assertEquals(10, result);
+    }
 }
